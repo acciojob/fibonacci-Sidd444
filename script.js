4474c8ef0da8ed32c179d8a4954df6284cc347cc
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-09 17:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+   let a=0,b=1;
+   if(num===0) return a;
+   if(num===1) return b;
+   for(let i=2;i<=num;i++){
+	  let curr=a+b;
+	  a=b;
+	  b=curr;
+   }
+   return curr;
 }
 
 module.exports = fibonacci;
