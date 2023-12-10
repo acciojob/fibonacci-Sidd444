db6fac603960ac295def69068ba3f87ce98b4057
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-10 22:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function fibonacci(num) {
    if(num===1) return 0;
    if(num===2) return 1;
    let a=0,b=1;
-   for(let i=2;i<=num;i++){
+   for(let i=2;i<num;i++){
 	  let curr=a+b;
 	  a=b;
 	  b=curr;
