2a9f7572c4abe0268ba0f0aa9f1baae12e117332
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-10 22:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
 // your code here
-   if(num===0) return 0;
-   if(num===1) return 1;
+   if(num===1) return 0;
+   if(num===2) return 1;
    let a=0,b=1;
    for(let i=2;i<=num;i++){
 	  let curr=a+b;
